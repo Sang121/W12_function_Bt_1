83aa35ca236f7cc554b1d19fd9b0f83fc9be25b0
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2023-02-17 18:30:14 +0700

    Bai tap 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState ,useEffect } from 'react';
 function App() {
+   const [num1, setNum1] = useState(0);
+   const [num2, setNum2] = useState(0);
+   const [sum,setSum]= useState(0);
+   const [mul, setMul]= useState(0);
+  
+  //  useEffect(() => {
+  //    setSum(num1+num2);  
+  //    setMul(num1 * num2);
+  //  }, [num1, num2]);
+  
+  const handleSum = () => {
+    setSum(num1+num2);
+  };
+  const handleMul = () => {
+    setMul(num1 * num2);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <div className='input'>
+      <div>
+        <p>  number 1:</p> 
+        <input type="number" value={num1} onChange={(e) => setNum1(e.target.value)} />
+      </div>
+      <div>
+      <p>  number 2:</p>
+      <input type="number" value={num2} onChange={(e) => setNum2(e.target.value)} />
+      </div>
+    </div>
+      <div  className="sum">
+        <button onClick={handleSum}> sum:{sum}</button>
+      
+        <button onClick={handleMul}> mul:{mul}</button>
+        </div>
+      
     </div>
   );
 }
